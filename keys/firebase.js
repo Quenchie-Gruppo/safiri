2d0cd528e9ecf83d06ef60ba68b306efda349160
ThaGreatFikshun <jaycomputertechnologies@gmail.com>
2025-04-18 22:34:04 +0300

    Updated minified json variable

--- keys/firebase.js
+++ keys/firebase.js
@@ -1,5 +1,28 @@
 // firebase.js
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./serviceAccountKey.json');
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// module.exports = admin;
+
+// firebase.js
 const admin = require('firebase-admin');
+const fs = require('fs');
+
+// Get JSON string from env
+const firebaseConfig = process.env.FIREBASE_CONFIG;
+
+if (!firebaseConfig) {
+  throw new Error('FIREBASE_CONFIG environment variable is missing');
+}
+
+// Write it to a temp file
+fs.writeFileSync('./serviceAccountKey.json', firebaseConfig);
+
+// Now require the written file
 const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
