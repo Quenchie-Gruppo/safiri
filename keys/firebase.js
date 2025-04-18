1751d0965be8c303e47f5476b7eb36a594d23c5c
ThaGreatFikshun <jaycomputertechnologies@gmail.com>
2025-04-19 00:43:41 +0300

    Updated environment variables

--- keys/firebase.js
+++ keys/firebase.js
@@ -1,32 +1,20 @@
-// firebase.js
-// const admin = require('firebase-admin');
-// const serviceAccount = require('./serviceAccountKey.json');
+// Load environment variables from .env file
+require('dotenv').config();
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
 
-// module.exports = admin;
-
-// firebase.js
-const admin = require('firebase-admin');
-const fs = require('fs');
-
-// Get JSON string from env
-const firebaseConfig = process.env.FIREBASE_CONFIG;
-
-if (!firebaseConfig) {
-  throw new Error('FIREBASE_CONFIG environment variable is missing');
-}
-
-// Write it to a temp file
-fs.writeFileSync('./serviceAccountKey.json', firebaseConfig);
-
-// Now require the written file
-const serviceAccount = require('./serviceAccountKey.json');
+const admin = require("firebase-admin");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  }),
 });
-
-module.exports = admin;
